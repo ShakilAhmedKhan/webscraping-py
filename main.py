0e741d3b327dff4e44412128307bb45b0a487509
Afsana Zaman Nipa <afsana2013nipa@gmail.com>
2024-01-07 00:19:01 +0600

    ok

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ with open('home.html', 'r') as html_file:
     soup = BeautifulSoup(content, 'lxml')
     #print(soup.prettify())
     tags = soup.find('h5') #chooses the first element only
-    tags = soup.find_all('h5') #chooses all element
-    print(tags)
+    # courses_html_tags = soup.find_all('h5') #chooses all element
+    # for course in courses_html_tags:
+    #     print(course.text)
+
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards:
+     print(course.h5.text)
+     print(course.a.text)
