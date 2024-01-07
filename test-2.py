fac295044348a0e7564816e4f9f44bf3c7f71603
Shakil Ahmed Khan <shakilcust@gmail.com>
2024-01-08 00:19:06 +0600

    3

--- test-2.py
+++ test-2.py
@@ -11,4 +11,18 @@ soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
 
 # print(jobs)
-print(job)
+company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+
+skill_rec = job.find('span', class_='srp-skills').text.replace(' ','')
+published_date = job.find('span', class_='sim-posted').text
+
+# print(skill_rec)
+# print(company_name.replace(' ', ''))
+# print(job.h3.text.replace(' ', ''))
+
+print(f''''
+
+Company Name: {company_name}
+Required Skills: {skill_rec}
+Published Date: {published_date}
+''')
