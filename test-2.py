23fb01aa2e36b5fa5a707c2e4ff3c47822ba65bf
Shakil Ahmed Khan <shakilcust@gmail.com>
2024-01-10 00:07:34 +0600

    function

--- test-2.py
+++ test-2.py
@@ -1,34 +1,44 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
 url_address = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation='
 
 #TODO: modify to input multiple values and match
 unfamiliar_skill = input('Put skills you do not have >')
-html_text = requests.get(url_address).text
-# print(html_text)
 
-soup = BeautifulSoup(html_text, 'lxml')
+def find_jobs():
+    html_text = requests.get(url_address).text
+    # print(html_text)
 
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-# job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+    soup = BeautifulSoup(html_text, 'lxml')
 
-# print(jobs)
-for job in jobs:
-    published_date = job.find('span', class_='sim-posted').text
-    if 'few' in published_date:
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    # job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
 
-        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+    # print(jobs)
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').text
+        if 'few' in published_date:
 
-        skill_rec = job.find('span', class_='srp-skills').text.replace(' ','')
-        more_info = job.header.h2.a['href']
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
 
-        # print(skill_rec)
-        # print(company_name.replace(' ', ''))
-        # print(job.h3.text.replace(' ', ''))
-        if unfamiliar_skill not in skill_rec:
-            print(f'Company Name: {company_name.strip()}')
-            print(f'Required Skills: {skill_rec.strip()}')
-            # print(f'Date: {published_date}')
-            print(f'More info: {more_info}')
+            skill_rec = job.find('span', class_='srp-skills').text.replace(' ','')
+            more_info = job.header.h2.a['href']
 
-            print(" ")
+            # print(skill_rec)
+            # print(company_name.replace(' ', ''))
+            # print(job.h3.text.replace(' ', ''))
+            if unfamiliar_skill not in skill_rec:
+                print(f'Company Name: {company_name.strip()}')
+                print(f'Required Skills: {skill_rec.strip()}')
+                # print(f'Date: {published_date}')
+                print(f'More info: {more_info}')
+
+                print(" ")
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_limit = 300
+        time.sleep(time_limit)
